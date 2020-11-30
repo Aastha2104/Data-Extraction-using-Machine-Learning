03aeb7206c6a02db2033913626f1f0970b4950a2
Aastha2104 <66529651+Aastha2104@users.noreply.github.com>
2020-11-30 21:44:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,89 @@
+import os
+import csv
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import text
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+
+
+DATA_PATH = "data\\"
+# FILE_NAME = "zomato_address_only.csv"
+# FILE_NAME = "zomato.csv"
+FILE_NAME = "address.csv"
+
+'''
+def add_Y_to_file(file_name):
+    file = open(file_name)
+    reader = csv.reader(file)
+    row0 = next(reader)
+    row0.append("Is_Address")
+
+    csv_input = pd.read_csv(FILE_NAME)
+    csv_input["Is Address"] =
+    csv_input.to_csv('output.csv', index=False)
+'''
+
+
+def main():
+    df_org = pd.read_csv(DATA_PATH + FILE_NAME)
+
+    # print(df_org["Address"])
+
+    target_data = df_org
+
+    # separate the training set into 80% for training and 20% for testing
+    msk = np.random.rand(len(target_data)) < 0.8
+    train_data = target_data[msk]
+    test_data = target_data[~msk]
+
+    # print(train_data["Address"])
+
+    #################### Naive Bayes Classifier Model ####################
+
+    vect = TfidfVectorizer()
+
+    X_train = vect.fit_transform(train_data["Address"])
+    X_test = vect.transform(test_data["Address"])
+
+    #################### Naive Bayes - Model ####################
+
+    text_clf_MNB = MultinomialNB()
+
+    # create an array of ones, whose length equal to the size of the train_data
+    # train_is_address = np.ones((len(train_data),), dtype=int)
+
+    text_clf_MNB.fit(X_train, train_data["Is address"])
+
+    predicted_NB = text_clf_MNB.predict(X_test)
+
+    # create an array of ones, whose length equal to the size of the test_data
+    # test_is_address = np.ones((len(test_data),), dtype=int)
+    NB_score = np.mean(predicted_NB == test_data["Is address"])
+
+    #################### Support Vector Machine - Model ####################
+
+    text_clf_SVM = SGDClassifier(loss='hinge', penalty='none', alpha=1e-3, random_state=42)
+
+    text_clf_SVM.fit(X_train, train_data["Is address"])
+    predicted_SVM = text_clf_SVM.predict(X_test)
+    SVM_score = np.mean(predicted_SVM == test_data["Is address"])
+
+    print(FILE_NAME)
+    print('Multinomial Naive Bayes (MNB)')
+    print('MNB_Score = ' + str(NB_score))
+
+    print('Support Vector Machine (SVM)')
+    print('SVM Score = ' + str(SVM_score))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,88 @@
+import os
+import argparse
+import numpy as np
+import pandas as pd
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import text
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+
+
+DATA_PATH = "data/"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+
+def parse_data(filename):
+    df_org = pd.read_csv(filename)
+    x = df_org["Address"]
+    y = df_org["Is address"]
+    return x, y
+
+
+def NB_classifier_model(train_xs, test_xs):
+    vect = TfidfVectorizer()
+    X_train = vect.fit_transform(train_xs)
+    X_test = vect.transform(test_xs)
+    return X_train, X_test
+
+
+# naive bayes classifier - feed the training data and predict the test data
+def NB_model(X_train, X_test, train_ys, test_ys):
+    text_clf_MNB = MultinomialNB()
+    text_clf_MNB.fit(X_train, train_ys)
+    predicted_NB = text_clf_MNB.predict(X_test)
+    score = np.mean(predicted_NB == test_ys)
+    return score
+
+
+# support vector machine - with given learning rate
+def SVM_model(X_train, X_test, train_ys, test_ys, args):
+    text_clf_SVM = SGDClassifier(loss='hinge', penalty='none', alpha=args.lr, random_state=42)
+    text_clf_SVM.fit(X_train, train_ys)
+    predicted_SVM = text_clf_SVM.predict(X_test)
+    score = np.mean(predicted_SVM == test_ys)
+    return score
+
+
+def main():
+
+    ########## args parser ##########
+
+    df_test_file = DATA_PATH + TRAIN_FILE_NAME
+
+    parser = argparse.ArgumentParser(description='Linear Classifier for Identify Addresses')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--test', type=str, default=df_test_file, help='test file')
+    args = parser.parse_args()
+
+    # training dataset
+    train_xs, train_ys = parse_data(DATA_PATH + args.test)
+
+    # testing dataset
+    test_xs, test_ys = parse_data(DATA_PATH + args.test)
+
+    # naive bayes classifer
+    X_train, X_test = NB_classifier_model(train_xs, test_xs)
+
+    # score for Naive Bayes
+    NB_score = NB_model(X_train, X_test, train_ys, test_ys)
+
+    # score for SVM
+    SVM_score = SVM_model(X_train, X_test, train_ys, test_ys, args)
+
+    print('\n-----------------------------')
+    print('Multinomial Naive Bayes (MNB)')
+    print('MNB_Score = ' + str(NB_score))
+
+    print('Support Vector Machine (SVM)')
+    print('SVM Score = ' + str(SVM_score))
+    print('-----------------------------\n')
+
+
+if __name__ == '__main__':
+    main()
